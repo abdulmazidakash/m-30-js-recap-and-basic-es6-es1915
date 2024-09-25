ce46ae085af69935b2a2c71a8bf8d831429cdcf8
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-25 22:33:52 +0600

    30-3 template string, multiple line string, dynamic string

---
+++ template.js
@@ -0,0 +1,26 @@
+const first = 'jaan';
+const last = 'pakhi';
+const greet = 'potas potas';
+
+const name = first + ' ' + last + ' ' + greet;
+console.log(name);
+
+const a = 10;
+const b = 20;
+
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a+b);
+console.log(result);
+
+const math = `The sum of ${a} and ${b} is ${a+b}`;
+console.log(math);
+
+const email = 'Hi john \n' +
+'cena cena lage \n' +
+'tumi mara mari korba naki';
+
+console.log(email);
+
+const challenge = `John cena
+cena naki ochena
+mair dimu prochur`;
+console.log(challenge);
